d366da743046471224aa28946c3c8bdd7a56cc1b
Samuel Wilson <samuel.wilson@fluxfederation.com>
2018-07-22 19:26:11 +1200

    Define tokens

---
+++ src/token/token.go
@@ -0,0 +1,30 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers and literals
+	IDENTIFIER = "IDENTIFIER"
+	INTEGER    = "INTEGER"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+
+	// Keywords
+	METHOD = "METHOD"
+)
