12a96da067074c7bae251b66ee447c9666458158
Samuel Wilson <samuel.wilson@fluxfederation.com>
2018-07-29 21:43:12 +1200

    Allow code to be interactively be read and printed

---
+++ src/repl/repl.go
@@ -0,0 +1,31 @@
+// repl interactively evaluates code.
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/samwson/ghost/src/lexer"
+	"github.com/samwson/ghost/src/token"
+	"io"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		lex := lexer.New(line)
+
+		for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
